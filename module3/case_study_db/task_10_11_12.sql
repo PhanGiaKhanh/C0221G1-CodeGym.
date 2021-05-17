e0e57c91f456457f80c0bd6c0e5528ae42705480
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-05-17 22:01:44 +0700

    up task 11

--- module3/case_study_db/task_10_11_12.sql
+++ module3/case_study_db/task_10_11_12.sql
@@ -12,13 +12,15 @@ order by so_luong desc;
 -- 11.	Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng bởi những Khách hàng có 
 -- TenLoaiKhachHang là “Diamond” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.
 select kh.ho_ten, dvdk.ten_dich_vu_di_kem, dvdk.gia, dvdk.don_vi, hdct.so_luong, kh.dia_chi, lk.ten_loai_khach
-from dich_vu_di_kem dvdk, hop_dong_chi_tiet hdct,  hop_dong hd, khach_hang kh, loai_khach lk
-where (dvdk.id_dich_vu_di_kem = hdct.id_dich_vu_di_kem
-and hdct.id_hop_dong = hd.id_hop_dong
-and hd.id_khach_hang = kh.id_khach_hang
-and kh.id_loai_khach = lk.id_loai_khach
-and kh.dia_chi in ("Vinh", "Quảng Ngãi")
-and lk.ten_loai_khach = "Diamond");
+from dich_vu_di_kem dvdk 
+join hop_dong_chi_tiet hdct on dvdk.id_dich_vu_di_kem = hdct.id_dich_vu_di_kem 
+join hop_dong hd on hdct.id_hop_dong = hd.id_hop_dong
+join khach_hang kh on hd.id_khach_hang = kh.id_khach_hang 
+join loai_khach lk on kh.id_loai_khach = lk.id_loai_khach
+where (
+	kh.dia_chi in ("Vinh", "Quảng Ngãi")
+	and lk.ten_loai_khach = "Diamond"
+);
 
 -- 12.	Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang, TenDichVu, SoLuongDichVuDikem
 -- (được tính dựa trên tổng Hợp đồng chi tiết), TienDatCoc của tất cả các dịch vụ đã từng được khách hàng đặt vào 
