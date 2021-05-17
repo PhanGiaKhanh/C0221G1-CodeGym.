e851d8a5ade135ab7e7c59af7799e727a1f29d43
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-05-17 16:33:16 +0700

    up

--- module3/case_study_db/task_16_17_18.sql
+++ module3/case_study_db/task_16_17_18.sql
@@ -15,20 +15,34 @@ where id_nhan_vien not in (
 -- 17.	Cập nhật thông tin những khách hàng có TenLoaiKhachHang từ  Platinium lên Diamond,
 -- chỉ cập nhật những khách hàng đã từng đặt phòng với tổng Tiền thanh toán trong năm 2019 
 -- là lớn hơn 10.000.000 VNĐ
-select kh.id_khach_hang, kh.ho_ten , sum(dv.chi_phi_thue + hdct.so_luong*dvdk.gia ) as tong_tien,year(hd.ngay_lam_hop_dong) as year_hd
-from khach_hang kh
+create view tong_tien_theo_thang_dv_dvdk as
+-- tổng tiền theo tháng dv
+select kh.ho_ten, lk.ten_loai_khach, month(ngay_lam_hop_dong) as months, hd.ngay_lam_hop_dong, sum(dv.chi_phi_thue) as tong_tien 
+from hop_dong hd 
+join dich_vu dv on dv.id_dich_vu = hd.id_dich_vu
+join khach_hang kh on kh.id_khach_hang = hd.id_khach_hang
 join loai_khach lk on lk.id_loai_khach = kh.id_loai_khach
-join hop_dong hd on hd.id_khach_hang = kh.id_khach_hang
-left join dich_vu dv on dv.id_dich_vu = hd.id_dich_vu
+group by kh.id_khach_hang
+union all
+-- tổng tiền theo thang dvdk
+select kh.ho_ten, lk.ten_loai_khach, month(ngay_lam_hop_dong) as months, hd.ngay_lam_hop_dong, sum(dvdk.gia*hdct.so_luong) as tong_tien
+from hop_dong hd
 join hop_dong_chi_tiet hdct on hdct.id_hop_dong = hd.id_hop_dong
-join dich_vu_di_kem dvdk on dvdk.id_dich_vu_di_kem = hdct.id_hop_dong_chi_tiet
-where (
-	year(hd.ngay_lam_hop_dong) =2019
-)
-group by hd.id_hop_dong
-having tong_tien > 10000000;
+join dich_vu_di_kem dvdk on dvdk.id_dich_vu_di_kem = hdct.id_dich_vu_di_kem
+join khach_hang kh on kh.id_khach_hang = hd.id_khach_hang
+join loai_khach lk on lk.id_loai_khach = kh.id_loai_khach
+group by kh.id_khach_hang;
 
--- 18.	Xóa những khách hàng có hợp đồng trước năm 2016 (chú ý ràngbuộc giữa các bảng).
+select ho_ten, ten_loai_khach, sum(tong_tien) as tong
+from tong_tien_theo_thang_dv_dvdk
+where (
+	year(ngay_lam_hop_dong) = 2019
+)
+group by months
+having tong > 10000000;
+
+
+-- 18.	Xóa những khách hàng có hợp đồng trước năm 2016 (chú ý ràng buộc giữa các bảng).
 delete
 from khach_hang
 where id_khach_hang in (
@@ -36,4 +50,5 @@ where id_khach_hang in (
     from hop_dong
     where year(ngay_lam_hop_dong) < 2016
 );
-
+-- sử dụng lệnh dưới cho các khóa ngoại >> xóa luôn các ràng buộc 
+--     on delete cascade
--- module3/case_study_db/task_19_20.sql
+++ module3/case_study_db/task_19_20.sql
@@ -1,8 +1,44 @@
 use case_study_db;
 
 -- 19.	Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.
+-- update 
+create view so_lan_su_dung_dvdk_2019 as
+	select dvdk.ten_dich_vu_di_kem, year(hd.ngay_lam_hop_dong) as years, sum(hdct.so_luong) as so_lan_su_dung
+    from dich_vu_di_kem dvdk
+    join hop_dong_chi_tiet hdct on dvdk.id_dich_vu_di_kem = hdct.id_dich_vu_di_kem
+    join hop_dong hd on hdct.id_hop_dong = hd.id_hop_dong
+    where year(hd.ngay_lam_hop_dong) = 2019
+    group by dvdk.ten_dich_vu_di_kem
+    having so_lan_su_dung >= 10;
 
+update dich_vu_di_kem 
+set gia = gia * 2
+where ten_dich_vu_di_kem in (
+	select ten_dich_vu_di_kem from so_lan_su_dung_dvdk_2019
+);
 
+-- cách 2     
+update dich_vu_di_kem 
+set gia = gia * 2
+where id_dich_vu_di_kem in (
+	select hdct.id_dich_vu_di_kem 
+    from hop_dong_chi_tiet hdct
+    join hop_dong hd on hdct.id_hop_dong = hd.id_hop_dong
+    where year(hd.ngay_lam_hop_dong) = 2019	
+    group by hdct.id_dich_vu_di_kem
+    having sum(hdct.so_luong) >= 10
+);
 -- 20.	Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống, 
 -- thông tin hiển thị bao gồm ID 
 -- (IDNhanVien, IDKhachHang), HoTen, Email, SoDienThoai, NgaySinh, DiaChi.
+select id_nhan_vien as id, ho_ten, email, sdt, ngay_sinh, dia_chi
+from nhan_vien nv
+union
+select id_khach_hang as id, ho_ten, email, sdt, ngay_sinh, dia_chi
+from khach_hang kh 
+
+
+
+
+
+
