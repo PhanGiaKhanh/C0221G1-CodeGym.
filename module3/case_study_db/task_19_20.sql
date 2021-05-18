a5c045dd2533a7a869f61164b131388f29669113
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-05-18 08:04:50 +0700

    fix task 19

--- module3/case_study_db/task_19_20.sql
+++ module3/case_study_db/task_19_20.sql
@@ -3,18 +3,18 @@ use case_study_db;
 -- 19.	Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.
 -- update 
 create view so_lan_su_dung_dvdk_2019 as
-	select dvdk.ten_dich_vu_di_kem, year(hd.ngay_lam_hop_dong) as years, sum(hdct.so_luong) as so_lan_su_dung
+	select dvdk.id_dich_vu_di_kem, year(hd.ngay_lam_hop_dong) as years, sum(hdct.so_luong) as so_lan_su_dung
     from dich_vu_di_kem dvdk
     join hop_dong_chi_tiet hdct on dvdk.id_dich_vu_di_kem = hdct.id_dich_vu_di_kem
     join hop_dong hd on hdct.id_hop_dong = hd.id_hop_dong
     where year(hd.ngay_lam_hop_dong) = 2019
-    group by dvdk.ten_dich_vu_di_kem
+    group by dvdk.id_dich_vu_di_kem
     having so_lan_su_dung >= 10;
 
 update dich_vu_di_kem 
 set gia = gia * 2
-where ten_dich_vu_di_kem in (
-	select ten_dich_vu_di_kem from so_lan_su_dung_dvdk_2019
+where id_dich_vu_di_kem in (
+	select id_dich_vu_di_kem from so_lan_su_dung_dvdk_2019
 );
 
 -- cách 2     
