3f9c7fe352f8bc983af0150347d6fd75a4b755f9
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-05-20 09:00:36 +0700

    up

--- module3/case_study_db/task_19_20.sql
+++ module3/case_study_db/task_19_20.sql
@@ -1,20 +1,7 @@
 use case_study_db;
 
 -- 19.	Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.
--- update 
-update dich_vu_di_kem 
-set gia = gia * 2
-where ten_dich_vu_di_kem in (
-	select a.ten_dich_vu_di_kem from (select dvdk.id_dich_vu_di_kem, year(hd.ngay_lam_hop_dong) as years, sum(hdct.so_luong) as so_lan_su_dung
-    from dich_vu_di_kem dvdk
-    join hop_dong_chi_tiet hdct on dvdk.id_dich_vu_di_kem = hdct.id_dich_vu_di_kem
-    join hop_dong hd on hdct.id_hop_dong = hd.id_hop_dong
-    where year(hd.ngay_lam_hop_dong) = 2019
-    group by dvdk.id_dich_vu_di_kem
-    having so_lan_su_dung >= 10) as a
-);	
-
--- cách 2     
+-- update    
 update dich_vu_di_kem 
 set gia = gia * 2
 where id_dich_vu_di_kem in (
