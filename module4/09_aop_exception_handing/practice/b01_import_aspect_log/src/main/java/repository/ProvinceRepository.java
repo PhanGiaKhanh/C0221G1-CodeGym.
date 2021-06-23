f1cf9e96b12a82bfca6092f55198472c6e112d20
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-06-23 10:24:23 +0700

    up

---
+++ module4/09_aop_exception_handing/practice/b01_import_aspect_log/src/main/java/repository/ProvinceRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import model.Province;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends CrudRepository<Province, Long> {
+}
