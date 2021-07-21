93046774909c0839cf14d601742a828396c1e972
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-07-22 05:57:03 +0700

    up

---
+++ module5/05_components_interaction/exercises/src/app/rating-bar-component/rating-bar-component.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-rating-bar-component',
+  templateUrl: './rating-bar-component.component.html',
+  styles: [
+  ]
+})
+export class RatingBarComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
