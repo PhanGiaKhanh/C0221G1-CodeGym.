cde7a2938b92ffe3ddabc9d184d861487a2ddffb
PhanGiaKhanh <phangiakhanh90@gmail.com>
2021-07-22 08:23:02 +0700

    up

--- module5/03_angular_overview/exercises/src/app/color/color.component.ts
+++ module5/03_angular_overview/exercises/src/app/color/color.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-color',
@@ -8,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ColorComponent implements OnInit {
   color = 'black';
   size = 20;
-  constructor() { }
+  red = 0;
+  blue = 0;
+  green = 0;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
